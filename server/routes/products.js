5fe5d2570d5713969ad01335f0efbe0614cc925f
Dinalisse <dfelicione@gmail.com>
2021-12-03 19:44:28 +0000

    feat: bootstrap styles

--- server/routes/products.js
+++ server/routes/products.js
@@ -3,7 +3,6 @@ const Product= require('../db/models/Product');
 
 router.get('/', async (req, res)=>{
   const products = await Product.findAll();
-  console.log(products);
   res.render('products', {products});
 });
 module.exports = router;
