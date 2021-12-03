3b63b89a24a449d8f9be56911e738b843af36bc0
Lucas Freitas <lufrei@google.com>
2021-12-03 18:44:25 +0000

    feat(seeding): Users should be able to be seeded now.

--- seed.js
+++ seed.js
@@ -5,10 +5,7 @@ const {Product, User, Order}= require('./server/db/index');
 
 // dont have User or order info to seed
 
-const seed = async ()=>{
-  // clear out our table
-  await db.sync({force: true});
-
+const seedProduct = async ()=>{
   // find the path to our json file
   const seedPath = path.join(__dirname, 'products.json');
 
@@ -22,5 +19,31 @@ const seed = async ()=>{
   console.log('product data has been successfully populated into our table');
 };
 
-// export this seed function
+
+const seedUser = async ()=>{
+  // find the path to our json file
+  const seedPath = path.join(__dirname, 'users.json');
+
+  const buffer = await fs.readFile(seedPath);
+  const users = JSON.parse(String(buffer));
+
+  // will create each row for our Product Table
+  const userPromises = users.map((user) => User.create(user));
+
+  await Promise.all(userPromises);
+  console.log('User data has been successfully populated into our table');
+};
+
+const seed = async () => {
+  console.log("seeding Database...");
+  await db.sync({force: true});
+  console.log("DB cleared");
+  await seedProduct();
+  await seedUser();
+  console.log("DB successfully seeded");
+}
+
+// export the seed function
+  // Why?
+
 seed();
