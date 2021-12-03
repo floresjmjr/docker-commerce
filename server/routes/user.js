ccb56832e0372382cef5f5da9df70161ba6d4794
Dinalisse <dfelicione@gmail.com>
2021-12-03 22:46:31 +0000

    fix: api route cleanup

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,16 +1,5 @@
-const router = require('express').Router()
+const router = require('express').Router();
 const {User} = require('../db');
-
-
-router.get('/:id', async (req, res) => {
-    try{
-        const user = await User.findByPk(req.params.id);
-        res.json(user);
-    } catch (err) {
-        console.error(err);
-    }
-})
-
 
 router.get('/signup', async (req, res)=>{
   res.render('signup');
@@ -19,6 +8,15 @@ router.get('/signup', async (req, res)=>{
 router.post('/signup', async (req, res)=>{
   const newUser = await User.create(req.body);
   res.send(`<h3>Thanks your all signed up</h3>`);
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    res.json(user);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 module.exports = router;
