03fe51a5120de05216bf3765ac88870445fa68b9
Dinalisse <dfelicione@gmail.com>
2021-12-04 23:03:33 +0000

    feat: server side validations for signup

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -3,10 +3,14 @@ const {db, DataTypes, Model}= require('../db');
 
 class User extends Model {
   // add methods here
+  static async findByEmail(email) {
+    const user= await User.findOne({where: {email: email}});
+    return user;
+  }
 }
 
 User.init({
-  type: DataTypes.STRING, //"Standard", "Admin"
+  type: DataTypes.STRING, // "Standard", "Admin"
   name: DataTypes.STRING,
   address: DataTypes.STRING,
   password: DataTypes.STRING,
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,11 +1,37 @@
+/* eslint-disable max-len */
 const router = require('express').Router();
+const {check, validationResult} = require('express-validator');
 const {User} = require('../db');
+
+const signupChecks = [
+  check('name')
+      .notEmpty().withMessage('name cannot be empty') // checks name is not empty
+      .bail() // sends an error if this fails with out checking others in list
+      .isLength({max: 50}).withMessage('name cannot exceed 50 chars') // max length can be 50
+      .bail()
+      .isAlpha('en-US', {ignore: ' '}).withMessage('name cannot contain number or symbols'), // can only have letters
+  check('email')
+      .isEmail() // checks if is email
+      .bail()
+      .custom(async (email)=>{ // checks if email is in use
+        const user = await User.findByEmail(email);
+        if (user) {
+          throw new Error('This email is already in use');
+        }
+      }),
+  check('password').notEmpty(), // want to make this stronger but the data seeeded will needd to match validations,
+];
+
 
 router.get('/signup', async (req, res)=>{
   res.render('signup');
 });
 
-router.post('/signup', async (req, res)=>{
+router.post('/signup', signupChecks, async (req, res)=>{
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array()});
+  }
   const newUser = await User.create(req.body);
   res.send(`<h3>Thanks your all signed up</h3>`);
 });
