5129b30bf97cfca4b568ef7e23bf31d9390e9ddb
Dinalisse <dfelicione@gmail.com>
2021-12-06 01:04:57 +0000

    fix: error message display

--- server/routes/user.js
+++ server/routes/user.js
@@ -6,11 +6,11 @@ const {User} = require('../db');
 
 const signupChecks = [
   check('name')
-      .notEmpty().withMessage('name cannot be empty') // checks name is not empty
+      .notEmpty().withMessage('Name cannot be empty') // checks name is not empty
       .bail() // sends an error if this fails with out checking others in list
-      .isLength({max: 50}).withMessage('name cannot exceed 50 chars') // max length can be 50
+      .isLength({max: 50}).withMessage('Name cannot exceed 50 chars') // max length can be 50
       .bail()
-      .isAlpha('en-US', {ignore: ' '}).withMessage('name cannot contain number or symbols'), // can only have letters
+      .isAlpha('en-US', {ignore: ' '}).withMessage('Name cannot contain numbers or symbols'), // can only have letters
   check('email')
       .isEmail() // checks if is email
       .bail()
@@ -20,7 +20,7 @@ const signupChecks = [
           throw new Error('This email is already in use');
         }
       }),
-  check('password').notEmpty(), // want to make this stronger but the data seeeded will needd to match validations,
+  check('password').notEmpty().withMessage('Password cannot be empty'), // want to make this stronger but the data seeeded will needd to match validations,
 ];
 // let validatationError;
 
