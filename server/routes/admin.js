e880f6823facbec7e91c6dd537c6de4e8675f090
Jorge F <jorge.m.flores@protonmail.com>
2021-12-07 05:27:37 +0000

    feat: can update and remove product

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,31 +2,26 @@ const router = require('express').Router();
 const {check, validationResult} = require('express-validator');
 const {Product} = require('../db/index');
 
+//Server-side validation checks for the product through the req.body
 const productChecks = [
-  check('title')
-      .notEmpty(),
-  check('price')
-      .notEmpty()
-      .isCurrency(),
-  check('description')
-      .notEmpty(),
-  check('category')
-      .notEmpty,
-  check('image')
-      .notEmpty()
-      .isURL(),
+  check('title').notEmpty(),
+  check('price').notEmpty().isCurrency(),
+  check('description').notEmpty(),
+  check('category').notEmpty(),
+  check('image').notEmpty().isURL(),
 ];
 
+//Admin views the page to create a new product
 router.get('/admin/newProduct', (req, res, next) => {
   try {
     // need to verify that person is admin
-    console.log(req);
     res.render('addProduct');
   } catch (error) {
     next(error);
   }
 });
 
+//Admin creates a new product for sale
 router.post('/admin/newProduct', productChecks, async (req, res, next) => {
   try {
     const errors = validationResult(req);
@@ -40,25 +35,22 @@ router.post('/admin/newProduct', productChecks, async (req, res, next) => {
   }
 });
 
+// Admin updates the product information
+router.put('/products/:productId/update', productChecks, async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(404).json({errors: errors.array()});
+    }
+    const product = await Product.findByPk(req.params.productId);
+    await product.update(req.body);
+    res.status(200).json({id: req.params.productId});
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.post(
-    '/updateproduct/:productId/update',
-    productChecks,
-    async (req, res, next) => {
-      try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-          return res.status(404).json({errors: errors.array()});
-        }
-        const product = await Product.findByPk(req.params.productId);
-        await product.update(req.body);
-        res.render('singleProduct', product);
-      } catch (error) {
-        next(error);
-      }
-    });
-
-
+//Admin views the page to update a product
 router.get('/products/:productId/update', async (req, res, next) => {
   try {
     const product = await Product.findByPk(req.params.productId);
@@ -68,11 +60,11 @@ router.get('/products/:productId/update', async (req, res, next) => {
   }
 });
 
-router.delete('/product/:productId', async (req, res, next) => {
+//Admin deletes a product from sale
+router.delete('/products/:productId', async (req, res, next) => {
   try {
     await Product.destroy({where: {id: req.params.productId}});
-    const products = await Product.findAll();
-    res.render('allProducts', products);
+    res.sendStatus(200);
   } catch (error) {
     next(error);
   }
