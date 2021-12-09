39c4165d16d77a6be1df9069acbd8b22280ba693
Dinalisse <dfelicione@gmail.com>
2021-12-09 15:37:22 +0000

    feat: webhook added to check payment confirmation

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -4,7 +4,20 @@ const {check, validationResult} = require('express-validator');
 const {User, Order, Product} = require('../db');
 require('dotenv').config();
 const stripe = require('stripe')('sk_test_51K4HnNDOuKMTHnuIRYjV7kV1IkeqMKQ3S9cPDR2Brk17Aktg7m7PIEKhssnXf2uX8pkuNURRl2PMKiV17iAty3DO00XkpYtpau');
+const express = require('express');
+const endpointSecret = 'whsec_zQX8PmpQ6nreCkT8AEWdt2X81jhMQ4K9';
 
+async function update(obj) {
+  const cart = await Order.findOne({
+    where: {
+      userId: obj,
+      isPurchased: 0,
+    },
+  });
+
+  cart.update({isPurchased: 1});
+  console.log('CART STATUS UPDATED');
+}
 
 router.get('/', async (req, res) => {
   let cartItems = await Order.findOne({
@@ -63,9 +76,52 @@ router.post('/create-checkout-session', async (req, res)=>{
     success_url: `${DOMAIN}/cart/success`,
     cancel_url: `${DOMAIN}/cart/cancel`,
   });
-  console.log('SESSION HERE', session);
+
   res.redirect(303, session.url);
-  console.log('SESSION HERE 2', session);
+});
+router.post('/webhook', express.json({type: 'application/json'}), (request, response) => {
+  const event = request.body;
+  // // Only verify the event if you have an endpoint secret defined.
+  // // Otherwise use the basic event deserialized with JSON.parse
+  // if (endpointSecret) {
+  //   // Get the signature sent by Stripe
+  //   const signature = request.headers['stripe-signature'];
+  //   try {
+  //     event = stripe.webhooks.constructEvent(
+  //         request.body,
+  //         signature,
+  //         endpointSecret,
+  //     );
+  //   } catch (err) {
+  //     console.log(`⚠️  Webhook signature verification failed.`, err.message);
+  //     return response.sendStatus(400);
+  //   }
+  // }
+
+  // Handle the event
+  switch (event.type) {
+    case 'payment_intent.succeeded': {
+      const paymentIntent = event.data.object;
+      console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`);
+      // Then define and call a method to handle the successful payment intent.
+      // handlePaymentIntentSucceeded(paymentIntent);
+      update(response.app.locals.user.id);
+    }
+
+
+      break;
+    // case 'payment_method.attached':
+    //   const paymentMethod = event.data.object;
+    //   // Then define and call a method to handle the successful attachment of a PaymentMethod.
+    //   // handlePaymentMethodAttached(paymentMethod);
+    //   break;
+    default:
+      // Unexpected event type
+      console.log(`Unhandled event type ${event.type}.`);
+  }
+
+  // Return a 200 response to acknowledge receipt of the event
+  response.send();
 });
 router.get('/success', async (req, res)=>{
   res.render('success');
